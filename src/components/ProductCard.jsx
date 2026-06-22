1a6c98a4c0f5c872cd5b0ec7a634ae116816292b
zingrin <jerinloncheu@gmail.com>
2026-06-22 13:16:26 +0600

    added products card section

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,22 +1,22 @@
-import { Link } from "react-router";
+import { Link } from "react-router-dom";
 
 const ProductCard = ({ product }) => {
   return (
-    <div className="bg-zinc-900 rounded-xl overflow-hidden">
+    <div className="bg-zinc-900 rounded-2xl overflow-hidden hover:-translate-y-2 transition duration-300">
       <img
         src={product.image}
         alt={product.name}
-        className="h-72 w-full object-cover"
+        className="w-full h-64 object-cover"
       />
 
       <div className="p-5">
         <h3 className="text-xl font-bold">{product.name}</h3>
 
-        <p className="text-yellow-500 mt-2">${product.price}</p>
+        <p className="text-yellow-500 text-lg mt-2">${product.price}</p>
 
         <Link
           to={`/product/${product.id}`}
-          className="inline-block mt-4 bg-yellow-500 text-black px-5 py-2 rounded"
+          className="inline-block mt-4 px-5 py-2 bg-yellow-500 text-black rounded-lg font-semibold"
         >
           View Details
         </Link>
